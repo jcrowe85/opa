63103bab9dd5095861a5ac403dec26faf47b368e
jcrow <jcrowe120485@gmail.com>
2024-04-11 22:02:57 -0700

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,35 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import "react-slideshow-image/dist/styles.css";
+import { Fade } from "react-slideshow-image";
+import bg1 from "/1.png";
+// import bg2 from "../assets/imgs/2.png";
+// import bg3 from "../assets/imgs/3.png";
+// import bg4 from "../assets/imgs/4.png";
+// import bg5 from "../assets/imgs/5.png";
+// import bg6 from "../assets/imgs/6.png";
+// import bg7 from "../assets/imgs/7.png";
+// import bg8 from "../assets/imgs/8.png";
+// import bg9 from "../assets/imgs/9.png";
+// import bg10 from "../assets/imgs/10.png";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div className={styles.container}>
+      <img src="/1.png" alt="Opa" />
+      <img src="/2.png" alt="Opa" />
+      <img src="/11.png" alt="Opa" />
+      <img src="/3.png" alt="Opa" />
+      <img src="/4.png" alt="Opa" />
+      <img src="/5.png" alt="Opa" />
+      <img src="/12.png" alt="Opa" />
+      <img src="/6.png" alt="Opa" />
+      <img src="/7.png" alt="Opa" />
+      {/* <img src="/8.png" alt="Opa" /> */}
+      <img src="/9.png" alt="Opa" />
+      <img src="/10.png" alt="Opa" />
+    </div>
   );
 }
